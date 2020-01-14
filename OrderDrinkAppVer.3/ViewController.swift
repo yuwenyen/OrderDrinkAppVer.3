43b4faccc94ae49952cf514e0b1bcd8d878f0e28
yuwenyen <u06m3jp600@gmail.com>
2020-01-15 00:30:06 +0900

    Initial Commit

---
+++ OrderDrinkAppVer.3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OrderDrinkAppVer.3
+//
+//  Created by 顏禹文 on 2020/01/15.
+//  Copyright © 2020 顏禹文. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ OrderDrinkAppVer.3Tests/OrderDrinkAppVer_3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  OrderDrinkAppVer_3Tests.swift
+//  OrderDrinkAppVer.3Tests
+//
+//  Created by 顏禹文 on 2020/01/15.
+//  Copyright © 2020 顏禹文. All rights reserved.
+//
+
+import XCTest
+@testable import OrderDrinkAppVer_3
+
+class OrderDrinkAppVer_3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
